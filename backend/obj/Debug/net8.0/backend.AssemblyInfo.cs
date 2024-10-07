67fceaae4ecff59297a4d030c22a6ca63d568a10
Matheuscrz <matheuslimasof.eng@gmail.com>
2024-10-07 14:06:04 -0300

    Arquivos de configs

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df3eb4f12618dc90a272725e0bb3b8ce006592ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83b6931682131933a914c0b88e2ed0bae989fe08")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
